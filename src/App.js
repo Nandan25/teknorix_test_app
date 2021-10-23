0c5cda91aaecd782cd3726dbdc437327bafa9183
Nandan <nandanprabhudesai@gmail.com>
2021-10-23 12:26:51 +0530

    resolved title issue

--- src/App.js
+++ src/App.js
@@ -10,26 +10,44 @@ function App() {
   const { departments, department_loading, department_error } =
     useFetchDepartments(params);
 
+  const buildFinalDataObject = () => {
+    if (
+      jobs &&
+      departments &&
+      !loading &&
+      !department_loading &&
+      !error &&
+      !department_error
+    ) {
+      const divARR = [];
+
+      departments.map((department, index) => {
+        department.jobs = [];
+        jobs.map((job) => {
+          if (job.function.title === department.title) {
+            department.jobs.push(job);
+          }
+        });
+      });
+
+      departments.map((department) => {
+        if (department.jobs.length > 0) {
+          divARR.push(<h1>{department.title}</h1>);
+          department.jobs.map((job) => {
+            divARR.push(<Job key={job.id} job={job} />);
+          });
+        }
+      });
+
+      return divARR;
+    }
+  };
+
   return (
     <Container>
-      {loading && <h1>Loading.....</h1>}
-      {error && <h1>Error.Try Refreshing</h1>}
-      {departments.map((department) => {
-        return (
-          <>
-            {jobs.map((job) => {
-              return job.function.title === department.title ? (
-                <>
-                  <h1>{department.title}</h1>
-                  <Job key={job.id} job={job} />
-                </>
-              ) : (
-                <></>
-              );
-            })}
-          </>
-        );
-      })}
+      {loading || (department_loading && <h1>Loading.....</h1>)}
+      {error || (department_error && <h1>Error.Try Refreshing</h1>)}
+      {buildFinalDataObject()}
     </Container>
   );
 }
